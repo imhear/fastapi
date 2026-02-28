ddbb6ff829a2066ae8c387e4f9cfabaa87221947
wutao <571934457@qq.com>
2026-02-28 22:31:56 +0800

    add sys_user.version column,sys_role.version column

--- app/modules/permission/models.py
+++ app/modules/permission/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Boolean, SmallInteger, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from app.models.base import Base, uuid_pk_column
+from app.models.base import BaseModel
 
 # 角色-权限关联表
 # sys_role_permission = Table(
@@ -10,16 +10,16 @@ from app.models.base import Base, uuid_pk_column
 #     Column('permission_id', ForeignKey('sys_permission.id'), primary_key=True),
 # )
 
-class SysPermission(Base):
+class SysPermission(BaseModel):
     __tablename__ = 'sys_permission'
 
-    id = uuid_pk_column()
+    # id = uuid_pk_column()
     code = Column(String(64), unique=True, nullable=False)      # 权限代码（如 user:create）
     name = Column(String(64), nullable=False)                   # 权限名称
     description = Column(String(255))                            # 描述
     category = Column(String(64), default='general')            # 分类
-    is_active = Column(Boolean, default=True)                    # 是否激活
-    status = Column(SmallInteger, default=1)
-    is_deleted = Column(SmallInteger, default=0)
+    # is_active = Column(Boolean, default=True)                    # 是否激活
+    # status = Column(SmallInteger, default=1)
+    # is_deleted = Column(SmallInteger, default=0)
 
     roles = relationship('SysRole', secondary='sys_role_permission', back_populates='permissions')
--- app/modules/user/api.py
+++ app/modules/user/api.py
@@ -1,13 +1,17 @@
+from typing import Any, Annotated
+
+from app.core.responses import ApiResponse
 from fastapi import APIRouter, Depends, HTTPException
 from dependency_injector.wiring import inject, Provide
 from app.di.container import Container
-from app.modules.user.schemas import UserCreate, UserResponse, UserProfileResponse
+from app.modules.user.schemas import UserCreate, UserResponse, UserProfileResponse, UserUpdate
 from app.modules.user.service import UserService
 from app.composers.user_detail import UserDetailComposer
 from app.core.exceptions import ResourceNotFound, BadRequest
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+UserServiceDep = Annotated[UserService, Depends(Provide[Container.user_container.user_service])]
 
 @router.get("/{user_id}/profile", response_model=UserProfileResponse)
 @inject
@@ -28,9 +32,52 @@ async def create_user(
     user_service: UserService = Depends(Provide[Container.user_container.user_service]),
 ):
     try:
+        # TODO add current_user.id to UserCreate.create_user
+        # TODO add current_user.id to UserCreate.update_user
         return await user_service.create_user(user_in)
     except BadRequest as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post(
+    "/update/{id}",
+    # response_model=ApiResponse[dict],
+    summary="更新用户信息",
+    description="更新用户信息并返回更新后的用户信息"
+)
+# @permission(
+#     code=PermissionCode.USER_UPDATE.value,
+#     name="用户更新权限",
+#     description="需要【user:update】权限"
+# )
+@inject
+async def update_user(
+        id: str,
+        user_update: UserUpdate,
+        # _superuser: CurrentSuperuser,
+        user_service: UserServiceDep,
+        # _=Depends(permission_checker(PermissionCode.USER_UPDATE.value))
+) -> Any:
+    """
+    更新用户信息
+    """
+    try:
+        print(f"🎯 API端点: 开始更新用户 {id}")
+        print(f"📨 请求数据: {user_update.model_dump(exclude_unset=True)}")
+
+        user_info = await user_service.update_user(id, user_update, user_update.version)
+        user_info.create_time = user_info.create_time.isoformat()
+        user_info.update_time = user_info.update_time.isoformat()
+        print(f"🎯 API端点: 开始返回用户 {user_info}")
+        return ApiResponse.success(data=id, msg="用户信息更新成功")
+        # return "用户信息更新成功"
+    except ResourceNotFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except BadRequest as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"用户信息更新失败: {str(e)}")
+
 
 @router.get("/{user_id}/detail")
 @inject
