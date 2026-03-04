2f13ff5983b73e29938989be23d77ac541db129d
wutao <571934457@qq.com>
2026-03-04 16:14:11 +0800

    fix nosence

--- app/modules/user/deps.py
+++
@@ -1,9 +0,0 @@
-"""
-API 依赖项配置文件
-backend/app/api/deps.py
-"""
-from app.core.auth import get_current_user
-from app.modules.user.models import SysUser
-from fastapi import Depends
-from typing import Annotated
-
